0ed8ef779c1de59e358e1ddaee9559dc3199ef05
Zenia Banerjee <zenia.banerjee@cloudkaptan.com>
2026-02-09 15:28:19 +0530

    fix: gitignore and pipeline updated

--- test/specs/google.e2e.ts
+++
@@ -1,9 +0,0 @@
-import GooglePage from "../pageobjects/google.page";
-
-describe("Google search", () => {
-  it("should search webdriverIO", async () => {
-    await GooglePage.open();
-    await GooglePage.search("webdriverio");
-    await expect(browser).toHaveTitle("WebdriverIO");
-  });
-});
